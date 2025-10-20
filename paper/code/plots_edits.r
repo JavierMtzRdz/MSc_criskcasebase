0269ce5a346f553c8f91fc2127511002895e3d4b
JavierMtzRdz <javierwolfsangel@gmail.com>
2025-10-20 16:11:05 -0700

    Update CIF and Brier Score plots with nicer format.

--- paper/code/plots_edits.r
+++ paper/code/plots_edits.r
@@ -1,19 +1,316 @@
-#Read data
 
-# run & save (as before)
-for (set in 1:5) brier_plot_fn(N, p, k, set)
+#generate title
 
-# load a saved run and tweak the plot label without recomputing
-run_id <- sprintf("setting%d_N%d_p%d_k%d", 2, N, p, k)
-res <- readRDS(here::here("paper","preds", paste0("results_", run_id, ".rds")))
+settings_tbl <- tibble::tribble(
+    ~setting,    ~desc,
+    "setting1",  "Single effects on end-point of interest",
+    "setting2", "Single effects on both end-points",
+    "setting3", "Opposing effects",
+    "setting4", "Mixture of effects",
+    "setting5", "Non-proportional hazards"
+)
 
-# edit labels and re-save the figure
-res$brier_table$title <- "New label here"
-p2 <- ggplot(res$brier_table, aes(times, Brier, colour = model)) +
-    geom_line(linewidth = 0.5) +
-    facet_grid(. ~ title) +
-    scale_colour_manual(values = c("#808080", "#D55E00", "#CC79A7", "#56B4E9", "#E69F00")) +
-    theme_bw()
+cpl_palette <- c("cbSCRIP" = "#277DA1", 
+                 "Aalen-Johansen" = "#43AA8B",
+                 "Boosted Fine-Gray" = "#F9C74F",
+                 "iCR" = "#f94144")
 
-ggsave(here::here("paper","figs", res$meta$fname), p2,
+## Set theme ------
+# mytidyfunctions::set_mytheme(text = element_text(family = "Times New Roman"))
+
+# Brier score plot ----
+
+N <- 400; p <- 120; k <- 20
+
+brier_table <- map_df(1:5,
+                      ~ {run_id <- sprintf("setting%d_N%d_p%d_k%d", ., N, p, k)
+                      res <- readRDS(here::here("paper","preds", 
+                                                paste0("results_", run_id, ".rds")))
+                      res$brier_table |> 
+                          mutate(setting = .)})
+
+(plot <- brier_table |> 
+    tibble() |> 
+    ggplot(aes(times, Brier, colour = model,
+               linewidth = ifelse(model == "cbSCRIP", 0.5, 0.1))) +
+    geom_line() +
+    facet_wrap(. ~ title, nrow = 2, scale = "free_x") +
+    scale_colour_manual(values = cpl_palette) +
+    scale_linewidth_continuous(range = c(0.5, 0.8),
+                               guide = "none") +
+    labs(x = "Follow-up time (years)",
+         color = "Models",
+         y = "Brier Score for Cause 1 Predictions") +
+    theme(legend.position = c(0.8, 0.2)))
+
+
+out_name   <- file.path(res_dir, sprintf("brier_grid_N%d_p%d_k%d.png", N, p, k))
+
+ggsave(out_name, plot,
+       width = 20, height = 12, units = "cm", dpi = 300)
+
+
+N <- 400; p <- 500; k <- 20
+
+brier_table <- map_df(1:5,
+                      ~ {run_id <- sprintf("setting%d_N%d_p%d_k%d", ., N, p, k)
+                      res <- readRDS(here::here("paper","preds", 
+                                                paste0("results_", run_id, ".rds")))
+                      res$brier_table |> 
+                          mutate(setting = .)})
+
+(plot <- brier_table |> 
+        tibble() |> 
+        ggplot(aes(times, Brier, colour = model,
+                   linewidth = ifelse(model == "cbSCRIP", 0.5, 0.1))) +
+        geom_line() +
+        facet_wrap(. ~ title, nrow = 2, scale = "free_x") +
+        scale_colour_manual(values = cpl_palette) +
+        scale_linewidth_continuous(range = c(0.5, 0.8),
+                                   guide = "none") +
+        labs(x = "Follow-up time (years)",
+             color = "Models",
+             y = "Brier Score for Cause 1 Predictions") +
+        theme(legend.position = c(0.8, 0.2)))
+
+
+out_name   <- file.path(res_dir, sprintf("brier_grid_N%d_p%d_k%d.png", N, p, k))
+
+ggsave(out_name, plot,
+       width = 20, height = 12, units = "cm", dpi = 300)
+
+
+N <- 400; p <- 500; k <- 84
+
+brier_table <- map_df(1:5,
+                      ~ {run_id <- sprintf("setting%d_N%d_p%d_k%d", ., N, p, k)
+                      res <- readRDS(here::here("paper","preds", 
+                                                paste0("results_", run_id, ".rds")))
+                      res$brier_table |> 
+                          mutate(setting = .)})
+
+(plot <- brier_table |> 
+        tibble() |> 
+        ggplot(aes(times, Brier, colour = model,
+                   linewidth = ifelse(model == "cbSCRIP", 0.5, 0.1))) +
+        geom_line() +
+        facet_wrap(. ~ title, nrow = 2, scale = "free_x") +
+        scale_colour_manual(values = cpl_palette) +
+        scale_linewidth_continuous(range = c(0.5, 0.8),
+                                   guide = "none") +
+        labs(x = "Follow-up time (years)",
+             color = "Models",
+             y = "Brier Score for Cause 1 Predictions") +
+        theme(legend.position = c(0.8, 0.2)))
+
+
+out_name   <- file.path(res_dir, sprintf("brier_grid_N%d_p%d_k%d.png", N, p, k))
+
+ggsave(out_name, plot,
+       width = 20, height = 12, units = "cm", dpi = 300)
+
+
+
+N <- 400; p <- 1000; k <- 20
+
+brier_table <- map_df(1:5,
+                      ~ {run_id <- sprintf("setting%d_N%d_p%d_k%d", ., N, p, k)
+                      res <- readRDS(here::here("paper","preds", 
+                                                paste0("results_", run_id, ".rds")))
+                      res$brier_table |> 
+                          mutate(setting = .)})
+
+(plot <- brier_table |> 
+        tibble() |> 
+        ggplot(aes(times, Brier, colour = model,
+                   linewidth = ifelse(model == "cbSCRIP", 0.5, 0.1))) +
+        geom_line() +
+        facet_wrap(. ~ title, nrow = 2, scale = "free_x") +
+        scale_colour_manual(values = cpl_palette) +
+        scale_linewidth_continuous(range = c(0.5, 0.8),
+                                   guide = "none") +
+        labs(x = "Follow-up time (years)",
+             color = "Models",
+             y = "Brier Score for Cause 1 Predictions") +
+        theme(legend.position = c(0.8, 0.2)))
+
+
+out_name   <- file.path(res_dir, sprintf("brier_grid_N%d_p%d_k%d.png", N, p, k))
+
+ggsave(out_name, plot,
+       width = 20, height = 12, units = "cm", dpi = 300)
+
+
+
+
+N <- 400; p <- 1000; k <- 168
+
+brier_table <- map_df(1:5,
+                      ~ {run_id <- sprintf("setting%d_N%d_p%d_k%d", ., N, p, k)
+                      res <- readRDS(here::here("paper","preds", 
+                                                paste0("results_", run_id, ".rds")))
+                      res$brier_table |> 
+                          mutate(setting = .)})
+
+(plot <- brier_table |> 
+        tibble() |> 
+        ggplot(aes(times, Brier, colour = model,
+                   linewidth = ifelse(model == "cbSCRIP", 0.5, 0.1))) +
+        geom_line() +
+        facet_wrap(. ~ title, nrow = 2, scale = "free_x") +
+        scale_colour_manual(values = cpl_palette) +
+        scale_linewidth_continuous(range = c(0.5, 0.8),
+                                   guide = "none") +
+        labs(x = "Follow-up time (years)",
+             color = "Models",
+             y = "Brier Score for Cause 1 Predictions") +
+        theme(legend.position = c(0.8, 0.2)))
+
+
+out_name   <- file.path(res_dir, sprintf("brier_grid_N%d_p%d_k%d.png", N, p, k))
+
+ggsave(out_name, plot,
+       width = 20, height = 12, units = "cm", dpi = 300)
+
+
+
+# CIF score plot ----
+
+N <- 400; p <- 120; k <- 20
+
+cif_table <- map_df(1:5,
+                      ~ {cif_data_fn(N, p, k, .) |> 
+                          mutate(setting = .)})
+
+(plot <- cif_table |> 
+        tibble() |> 
+        mutate(title = settings_tbl$desc[setting]) |> 
+        ggplot(aes(Time, Risk, colour = Method,
+                   linewidth = ifelse(Method == "cbSCRIP", 0.5, 0.1))) +
+        geom_line() +
+        facet_wrap(. ~ title, nrow = 2, scale = "free_x") +
+        scale_colour_manual(values = cpl_palette) +
+        scale_linewidth_continuous(range = c(0.5, 0.8),
+                                   guide = "none") +
+        labs(x = "Follow-up time (years)",
+             color = "Models",
+             y = "Absolute Risk") +
+        theme(legend.position = c(0.8, 0.2)))
+
+
+out_name   <- file.path(res_dir, sprintf("cif_grid_N%d_p%d_k%d.png", N, p, k))
+
+ggsave(out_name, plot,
+       width = 20, height = 12, units = "cm", dpi = 300)
+
+N <- 400; p <- 500; k <- 20
+
+cif_table <- map_df(1:5,
+                    ~ {cif_data_fn(N, p, k, .) |> 
+                            mutate(setting = .)})
+
+(plot <- cif_table |> 
+        tibble() |> 
+        mutate(title = settings_tbl$desc[setting]) |> 
+        ggplot(aes(Time, Risk, colour = Method,
+                   linewidth = ifelse(Method == "cbSCRIP", 0.5, 0.1))) +
+        geom_line() +
+        facet_wrap(. ~ title, nrow = 2, scale = "free_x") +
+        scale_colour_manual(values = cpl_palette) +
+        scale_linewidth_continuous(range = c(0.5, 0.8),
+                                   guide = "none") +
+        labs(x = "Follow-up time (years)",
+             color = "Models",
+             y = "Absolute Risk") +
+        theme(legend.position = c(0.8, 0.2)))
+
+
+out_name   <- file.path(res_dir, sprintf("cif_grid_N%d_p%d_k%d.png", N, p, k))
+
+ggsave(out_name, plot,
+       width = 20, height = 12, units = "cm", dpi = 300)
+
+
+N <- 400; p <- 500; k <- 84
+
+cif_table <- map_df(1:5,
+                    ~ {cif_data_fn(N, p, k, .) |> 
+                            mutate(setting = .)})
+
+(plot <- cif_table |> 
+        tibble() |> 
+        mutate(title = settings_tbl$desc[setting]) |> 
+        ggplot(aes(Time, Risk, colour = Method,
+                   linewidth = ifelse(Method == "cbSCRIP", 0.5, 0.1))) +
+        geom_line() +
+        facet_wrap(. ~ title, nrow = 2, scale = "free_x") +
+        scale_colour_manual(values = cpl_palette) +
+        scale_linewidth_continuous(range = c(0.5, 0.8),
+                                   guide = "none") +
+        labs(x = "Follow-up time (years)",
+             color = "Models",
+             y = "Absolute Risk") +
+        theme(legend.position = c(0.8, 0.2)))
+
+
+out_name   <- file.path(res_dir, sprintf("cif_grid_N%d_p%d_k%d.png", N, p, k))
+
+ggsave(out_name, plot,
+       width = 20, height = 12, units = "cm", dpi = 300)
+
+
+N <- 400; p <- 1000; k <- 20
+
+cif_table <- map_df(1:5,
+                    ~ {cif_data_fn(N, p, k, .) |> 
+                            mutate(setting = .)})
+
+(plot <- cif_table |> 
+        tibble() |> 
+        mutate(title = settings_tbl$desc[setting]) |> 
+        ggplot(aes(Time, Risk, colour = Method,
+                   linewidth = ifelse(Method == "cbSCRIP", 0.5, 0.1))) +
+        geom_line() +
+        facet_wrap(. ~ title, nrow = 2, scale = "free_x") +
+        scale_colour_manual(values = cpl_palette) +
+        scale_linewidth_continuous(range = c(0.5, 0.8),
+                                   guide = "none") +
+        labs(x = "Follow-up time (years)",
+             color = "Models",
+             y = "Absolute Risk") +
+        theme(legend.position = c(0.8, 0.2)))
+
+
+out_name   <- file.path(res_dir, sprintf("cif_grid_N%d_p%d_k%d.png", N, p, k))
+
+ggsave(out_name, plot,
+       width = 20, height = 12, units = "cm", dpi = 300)
+
+
+
+N <- 400; p <- 1000; k <- 168
+
+cif_table <- map_df(1:5,
+                    ~ {cif_data_fn(N, p, k, .) |> 
+                            mutate(setting = .)})
+
+(plot <- cif_table |> 
+        tibble() |> 
+        mutate(title = settings_tbl$desc[setting]) |> 
+        ggplot(aes(Time, Risk, colour = Method,
+                   linewidth = ifelse(Method == "cbSCRIP", 0.5, 0.1))) +
+        geom_line() +
+        facet_wrap(. ~ title, nrow = 2, scale = "free_x") +
+        scale_colour_manual(values = cpl_palette) +
+        scale_linewidth_continuous(range = c(0.5, 0.8),
+                                   guide = "none") +
+        labs(x = "Follow-up time (years)",
+             color = "Models",
+             y = "Absolute Risk") +
+        theme(legend.position = c(0.8, 0.2)))
+
+
+out_name   <- file.path(res_dir, sprintf("cif_grid_N%d_p%d_k%d.png", N, p, k))
+
+ggsave(out_name, plot,
        width = 20, height = 12, units = "cm", dpi = 300)
--- paper/code/prediction_functions.r
+++ paper/code/prediction_functions.r
@@ -96,38 +96,90 @@ predictRisk.iCoxBoost <- function(object, newdata, times, cause, ...) {
 
 
 
+# predictRisk.penalizedCompRisk <- function(object, newdata, times, cause, ...) {
+#     #get all covariates excluding intercept and time
+#     #newdata = newdata[, .SD, .SDcols = grep("X", colnames(newdata), value = TRUE)]
+#     if (missing(cause)) stop("Argument cause should be the event type for which we predict the absolute risk.")
+#     # the output of absoluteRisk is an array with dimension depending on the length of the requested times:
+#     # case 1: the number of time points is 1
+#     #         dim(array) =  (length(time), NROW(newdata), number of causes in the data)
+#     if (length(times) == 1) {
+#         a <- absoluteRisk.penalized(object, newdata = newdata, time = times, addZero = FALSE)
+#         p <- matrix(a, ncol = 1)
+#     } else {
+#         # case 2 a) zero is included in the number of time points
+#         if (0 %in% times) {
+#             # dim(array) =  (length(time)+1, NROW(newdata)+1, number of causes in the data)
+#             a <- absoluteRisk.penalized(object, newdata = newdata, time = times)
+#             p <- t(a)
+#         } else {
+#             # case 2 b) zero is not included in the number of time points (but the absoluteRisk function adds it)
+#             a <- absoluteRisk.penalized(object, newdata = newdata, time = times)
+#             ### we need to invert the plot because, by default, we get cumulative incidence
+#             #a[, -c(1)] <- 1 - a[, -c(1)]
+#             ### we remove time 0 for everyone, and remove the time column
+#             a <- a[-c(1), -c(1)] ### a[-c(1), ] to keep times column, but remove time 0 probabilities
+#             # now we transpose the matrix because in riskRegression we work with number of
+#             # observations in rows and time points in columns
+#             p <- t(a)
+#         }
+#     }
+#     if (NROW(p) != NROW(newdata) || NCOL(p) != length(times)) {
+#         stop(paste("\nPrediction matrix has wrong dimensions:\nRequested newdata x times: ", 
+#                    NROW(newdata), " x ", length(times), "\nProvided prediction matrix: ", 
+#                    NROW(p), " x ", NCOL(p), "\n\n", sep = ""))
+#     }
+#     p
+# }
+
+`%||%` <- function(a,b) if (!is.null(a)) a else b
+
 predictRisk.penalizedCompRisk <- function(object, newdata, times, cause, ...) {
-    #get all covariates excluding intercept and time
-    #newdata = newdata[, .SD, .SDcols = grep("X", colnames(newdata), value = TRUE)]
-    if (missing(cause)) stop("Argument cause should be the event type for which we predict the absolute risk.")
-    # the output of absoluteRisk is an array with dimension depending on the length of the requested times:
-    # case 1: the number of time points is 1
-    #         dim(array) =  (length(time), NROW(newdata), number of causes in the data)
-    if (length(times) == 1) {
-        a <- absoluteRisk.penalized(object, newdata = newdata, time = times, addZero = FALSE)
-        p <- matrix(a, ncol = 1)
+    stopifnot(cause == 1)
+    
+    cb <- object$cb_data
+    cn <- colnames(cb$covariates)
+    Xnew <- as.matrix(newdata[, cn, drop = FALSE])
+    N <- nrow(Xnew); Tt <- length(times)
+    
+    # --- grab and align coefficients by name ---
+    beta_mat <- object$coefficients
+    # drop intercept if present
+    if (!is.null(rownames(beta_mat)) && "(Intercept)" %in% rownames(beta_mat)) {
+        beta_mat <- beta_mat[setdiff(rownames(beta_mat), "(Intercept)"), , drop = FALSE]
+    }
+    # choose the column you want (here first)
+    if (is.matrix(beta_mat)) beta_mat <- beta_mat[, 1, drop = FALSE]
+    
+    # build beta vector matching covariate columns; fill absent ones with 0
+    if (!is.null(rownames(beta_mat))) {
+        beta <- setNames(rep(0, length(cn)), cn)
+        rn <- intersect(rownames(beta_mat), cn)
+        beta[rn] <- as.numeric(beta_mat[rn, 1])
+        beta <- as.numeric(beta)  # now length(beta) == length(cn)
     } else {
-        # case 2 a) zero is included in the number of time points
-        if (0 %in% times) {
-            # dim(array) =  (length(time)+1, NROW(newdata)+1, number of causes in the data)
-            a <- absoluteRisk.penalized(object, newdata = newdata, time = times)
-            p <- t(a)
-        } else {
-            # case 2 b) zero is not included in the number of time points (but the absoluteRisk function adds it)
-            a <- absoluteRisk.penalized(object, newdata = newdata, time = times)
-            ### we need to invert the plot because, by default, we get cumulative incidence
-            #a[, -c(1)] <- 1 - a[, -c(1)]
-            ### we remove time 0 for everyone, and remove the time column
-            a <- a[-c(1), -c(1)] ### a[-c(1), ] to keep times column, but remove time 0 probabilities
-            # now we transpose the matrix because in riskRegression we work with number of
-            # observations in rows and time points in columns
-            p <- t(a)
-        }
+        # no names available → last resort: require matching length
+        stopifnot(length(beta_mat) == length(cn))
+        beta <- as.numeric(beta_mat)
     }
-    if (NROW(p) != NROW(newdata) || NCOL(p) != length(times)) {
-        stop(paste("\nPrediction matrix has wrong dimensions:\nRequested newdata x times: ", 
-                   NROW(newdata), " x ", length(times), "\nProvided prediction matrix: ", 
-                   NROW(p), " x ", NCOL(p), "\n\n", sep = ""))
+    
+    # --- map requested times to the cb grid + offsets ---
+    gTimes <- sort(unique(cb$time))
+    off_by_time <- tapply(cb$offset, cb$time, mean)
+    pos <- prodlim::sindex(jump.times = gTimes, eval.times = pmin(times, max(gTimes)))
+    off_t <- as.numeric(off_by_time)[pos]  # length Tt
+    
+    # --- hazard and CIF (single cause) ---
+    linp <- as.vector(Xnew %*% beta)
+    hmat <- sapply(off_t, function(o) plogis(linp + o))  # N x Tt
+    
+    CIF <- matrix(0, N, Tt); S <- matrix(1, N, Tt)
+    for (j in seq_len(Tt)) {
+        add <- if (j == 1) hmat[, j] else S[, j-1] * hmat[, j]
+        CIF[, j] <- if (j == 1) add else CIF[, j-1] + add
+        S[, j]   <- if (j == 1) (1 - hmat[, j]) else S[, j-1] * (1 - hmat[, j])
     }
-    p
+    CIF <- pmin(1, pmax(0, CIF))
+    
+    return(CIF)
 }
